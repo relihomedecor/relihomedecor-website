953de8e225ed7cf1eefde234168307c584e14dcd
Qingbao <qingbao86@gmail.com>
2021-01-27 11:56:30 +0100

    Update homepage

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -3,7 +3,7 @@ import Twitter from "../assets/twitter-alt.svg";
 import GitHub from "../assets/github-alt.svg";
 import config from "../lib/config";
 
-export function SocialList({}) {
+export function SocialList({ }) {
   return (
     <div>
       <a
@@ -14,14 +14,14 @@ export function SocialList({}) {
       >
         <Twitter width={24} height={24} fill={"#222"} />
       </a>
-      <a
+      {/* <a
         title="GitHub"
         href={`https://github.com/${config.github_account}`}
         target="_blank"
         rel="noopener"
       >
         <GitHub width={24} height={24} fill={"#222"} />
-      </a>
+      </a> */}
       <style jsx>{`
         a {
           display: inline-block;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, We're Relihomedecor<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@relihomedecor</span>
+          <h2>A reliable home decor you can trust.</h2>
           <SocialList />
         </div>
       </div>
